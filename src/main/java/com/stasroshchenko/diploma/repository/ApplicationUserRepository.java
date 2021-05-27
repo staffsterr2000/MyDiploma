9330554fa8203cffcaf2839875bb96f6c60bb3a9
Stanislav Roshchenko <rosenkostas@gmail.com>
2021-05-27 17:49:14 +0300

    Ignore case query has been added.

--- src/main/java/com/stasroshchenko/diploma/repository/ApplicationUserRepository.java
+++ src/main/java/com/stasroshchenko/diploma/repository/ApplicationUserRepository.java
@@ -2,6 +2,7 @@ package com.stasroshchenko.diploma.repository;
 
 import com.stasroshchenko.diploma.entity.database.ApplicationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,7 +11,12 @@ import java.util.Optional;
 public interface ApplicationUserRepository
         extends JpaRepository<ApplicationUser, Integer> {
 
+    // ignores case
+    @Query("SELECT au FROM ApplicationUser au WHERE LOWER(au.username) = LOWER(?1)")
     Optional<ApplicationUser> findByUsername(String username);
 
+    // ignores case
+    @Query("SELECT au FROM ApplicationUser au WHERE LOWER(au.email) = LOWER(?1)")
     Optional<ApplicationUser> findByEmail(String email);
+
 }
